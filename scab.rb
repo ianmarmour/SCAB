23c4febcc4d5cd955f57545dce03e107b74cdad9
root <root@localhost.localdomain>
2017-01-04 16:35:31 +0000

    Initial scanning/sorting code, early stages

---
+++ scab.rb
@@ -0,0 +1,48 @@
+@iplist = Hash.new(0)
+filename = "outputlist.txt"
+
+Dir['/root/SCAB/*'].each do |file_name|
+  next if File.directory? file_name
+  if file_name.include? "outputlist.txt"
+    filename = "outputlistnew.txt"
+  end
+end
+
+%x(masscan 149.174.97.0/24 -p0,80 --ping --rate 10000 -oG #{filename})
+
+File.open("#{filename}", "r").each_line do |line|
+    data = line.match(/\d+\.\d+\.\d+.\d+/)
+    data = data.to_s
+    if (data != nil) && (data != "")
+      @iplist[data] += 1
+    end
+end
+
+#Creates parsed output lists
+if filename == "outputlistnew.txt"
+  File.open('parsedipsnew.txt', 'w') { |file|
+    @iplist.each do |key|
+      data = key.to_s
+      data = data.match(/\d+\.\d+\.\d+.\d+/)
+      file.puts data
+    end
+ }
+
+else
+  File.open('parsedips.txt', 'w') { |file|
+    @iplist.each do |key|
+      data = key.to_s
+      data = data.match(/\d+\.\d+\.\d+.\d+/)
+      file.puts data
+    end
+  }
+end
+
+if filename == "outputlistnew.txt" 
+  f1 = IO.readlines("parsedips.txt").map(&:chomp)
+  f2 = IO.readlines("parsedipsnew.txt").map(&:chomp)
+  File.open("deltas.txt","w"){ |f|
+    f.write((f1-f2).join("\n"))
+  }
+end
+
